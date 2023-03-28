6d09a0259c698faf007e9dbfb0e3fde4c81a410b
Wiktoria Kazmierczyk <wiktoriakazmierczyk@protonmail.com>
2023-03-28 19:01:27 +0200

    OpenGraph and emojis added

--- js/script.js
+++ js/script.js
@@ -19,10 +19,10 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
-            </li>
-            `;
+            </li >
+        `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
@@ -30,6 +30,24 @@
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if(newTaskContent === "") {
+            return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+        });
 
     };
 
