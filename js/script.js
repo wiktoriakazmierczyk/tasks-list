8690535c82fc436f33ea9546611e7aafb78cd452
Wiktoria Kazmierczyk <wiktoriakazmierczyk@protonmail.com>
2023-04-01 12:55:10 +0200

    Remove tasks

--- js/script.js
+++ js/script.js
@@ -14,26 +14,41 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
-            </li >
-        `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+
+            <button class="js-remove">usuń</button>
+            ${task.content}
+            </li >
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
     const onFormSubmit = ((event) => {
